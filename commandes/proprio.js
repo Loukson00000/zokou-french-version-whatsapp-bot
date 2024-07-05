b5ef9c9d358118d5331c3345dd39c8f4535abc3e
Loukson <123784461+Loukson@users.noreply.github.com>
2024-07-05 07:14:27 +0000

    Update proprio.js

--- commandes/proprio.js
+++ commandes/proprio.js
@@ -6,7 +6,7 @@ const {zokou} =require("../framework/zokou")
 
 
 
-zokou({nomCom:"redemarrer",categorie:"Mods",reaction:"👨🏿‍💼"},async(dest,z,com)=>{
+zokou({nomCom:"redemarrer",categorie:"Mods",reaction:"🤘🏻"},async(dest,z,com)=>{
 
 
   
@@ -14,7 +14,7 @@ const{repondre,ms,dev,superUser}=com;
 
   if(!superUser)
   {
-    return repondre("Cette commande est réservée au propriétaire du bot");
+    return repondre("désolé man🤘🏻 Cette commande est réservée au propriétaire du bot🤘🏻");
   }
 
   const {exec}=require("child_process") ;
